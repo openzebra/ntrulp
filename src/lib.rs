858a9e1de81f480d1f12775d5a5c4cd82feadcf4
Hicaru <hicc0rus@gmail.com>
2023-07-10 11:44:33 +0500

    added test for start params

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
 mod config;
 mod math;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use math::finite_field::GF;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let (round1, p, q, w) = config::params::SNTRUP4591761;
+        let hash_bytes: [u8; 0] = [];
+        let mut usecache = !round1;
+
+        assert!(math::prime::is_prime(p));
+        assert!(math::prime::is_prime(q));
+        assert!(w > 0);
+        assert!(2 * p >= 3 * w);
+        assert!(q >= 16 * w + 1);
+        assert!(q % 6 == 1); // spec allows 5 but these tests do not
+        assert!(p % 4 == 1); // spec allows 3 but ref C code does not
+
+        if round1 {
+            // encodings defined only for (761,4591)
+            usecache = false;
+            assert!(p == config::params::SNTRUP4591761.1);
+            assert!(q == config::params::SNTRUP4591761.2);
+        }
+
+        let f3 = GF::new(1, 3);
     }
 }
