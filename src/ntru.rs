21b7c0e02066e387848d448414dd9b341cff4632
Hicaru <hicc0rus@gmail.com>
2023-07-23 14:21:29 +0500

    remake init NTRU

--- src/ntru.rs
+++ src/ntru.rs
@@ -3,6 +3,9 @@ use crate::config::params::StartParams;
 use crate::math;
 use crate::math::finite_field::GF;
 
+use std::io::{Error, ErrorKind};
+
+#[derive(Debug)]
 pub struct NTRU {
     pub params: StartParams,
     hash_bytes: Vec<u8>, // TODO: change to ARC<[u8]>
@@ -12,61 +15,120 @@ pub struct NTRU {
 }
 
 impl NTRU {
-    pub fn from(params: StartParams) -> Self {
+    pub fn from(params: StartParams) -> Result<Self, Error> {
         let (round1, p, q, w) = params;
         let hash_bytes = vec![];
         let mut usecache = !round1;
 
-        assert!(math::prime::is_prime(p));
-        assert!(math::prime::is_prime(q));
-        assert!(w > 0);
-        assert!(2 * p >= 3 * w);
-        assert!(q >= 16 * w + 1);
-        assert!(q % 6 == 1); // spec allows 5 but these tests do not
-        assert!(p % 4 == 1); // spec allows 3 but ref C code does not
+        if !math::prime::is_prime(p) {
+            return Err(Error::new(ErrorKind::Other, "p must be prime number"));
+        }
+        if !math::prime::is_prime(q) {
+            return Err(Error::new(ErrorKind::Other, "q must be prime number"));
+        }
+        if !(w > 0) {
+            return Err(Error::new(ErrorKind::Other, "w cannot be < 0"));
+        }
+        if !(2 * p >= 3 * w) {
+            return Err(Error::new(ErrorKind::Other, "2*p should be >= 3*w"));
+        }
+        if !(q >= 16 * w + 1) {
+            return Err(Error::new(ErrorKind::Other, "q should be >= 17 * w + 1"));
+        }
+        if !(q % 6 == 1) {
+            // spec allows 5 but these tests do not
+            return Err(Error::new(ErrorKind::Other, "q mod 6 should be = 1"));
+        }
+        if !(p % 4 == 1) {
+            // spec allows 3 but ref C code does not
+            return Err(Error::new(ErrorKind::Other, "p mod 4 should be = 1"));
+        }
 
         if round1 {
             // encodings defined only for (761,4591)
             usecache = false;
-            assert!(p == config::params::SNTRUP4591761.1);
-            assert!(q == config::params::SNTRUP4591761.2);
+            if !(p == config::params::SNTRUP4591761.1) {
+                return Err(Error::new(ErrorKind::Other, "for round1 p should be 761"));
+            }
+            if !(q == config::params::SNTRUP4591761.2) {
+                return Err(Error::new(ErrorKind::Other, "for round1 p should be 4591"));
+            }
         }
 
         let fq = GF::new(1, q as u64);
         let q12 = (q - 1) / 2;
 
-        NTRU {
+        Ok(NTRU {
             params,
             hash_bytes,
             usecache,
             fq,
             q12,
-        }
-    }
-
-    pub fn zz_from_ff(&self, c: u64, ff: &GF<u64>) -> u64 {
-        assert!(ff.has(c), "Element must be in GF(ff)");
-
-        let pff = ff.p;
-        let value = GF::new(c.wrapping_add(1), pff).v;
-
-        value.wrapping_sub(value)
-    }
-
-    pub fn zz_from_fq(&self, c: u64) -> u64 {
-        let (_, _, q, _) = self.params;
-        let q = q as u64;
-
-        assert!(c < q, "Element must be in Fq");
-
-        let q12 = self.q12 as u64;
-        let sum = c.wrapping_add(q12);
-
-        sum.wrapping_sub(q12)
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_valid_params() {
+        let result = NTRU::from(config::params::SNTRUP4591761);
+        assert!(result.is_ok());
+        let result = NTRU::from(config::params::SNTRUP761);
+        assert!(result.is_ok());
+        let result = NTRU::from(config::params::SNTRUP653);
+        assert!(result.is_ok());
+        let result = NTRU::from(config::params::SNTRUP857);
+        assert!(result.is_ok());
+        let result = NTRU::from(config::params::SNTRUP953);
+        assert!(result.is_ok());
+        let result = NTRU::from(config::params::SNTRUP1013);
+        assert!(result.is_ok());
+        let result = NTRU::from(config::params::SNTRUP1277);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_invalid_p_not_prime() {
+        let invalid_params: StartParams = (false, 15, 4591, 135);
+        let result = NTRU::from(invalid_params);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_invalid_q_not_prime() {
+        let invalid_params: StartParams = (false, 761, 20, 135);
+        let result = NTRU::from(invalid_params);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_invalid_w_negative() {
+        let invalid_params: StartParams = (false, 761, 4591, 0);
+        let result = NTRU::from(invalid_params);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_invalid_2p_not_greater_than_3w() {
+        let invalid_params: StartParams = (false, 10, 8, 4);
+        let result = NTRU::from(invalid_params);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_invalid_q_mod_6_not_1() {
+        let invalid_params: StartParams = (false, 761, 4590, 135);
+        let result = NTRU::from(invalid_params);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_invalid_p_mod_4_not_1() {
+        let invalid_params: StartParams = (false, 14, 4591, 135);
+        let result = NTRU::from(invalid_params);
+        assert!(result.is_err());
+    }
 }
