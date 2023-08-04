29981f74135e87d38603dcca470e3160cbcae692
Hicaru <hicc0rus@gmail.com>
2023-08-04 12:54:40 +0500

    added enc_len

--- src/ntrup.rs
+++ src/ntrup.rs
@@ -1,6 +1,5 @@
 use crate::config::params::StartParams;
 use crate::math;
-use crate::math::finite_field::GF;
 use crate::{config, key::pair::NtruPrimeKeyPair};
 
 use std::io::{Error, ErrorKind};
@@ -35,10 +34,6 @@ impl NTRUPrime {
             // spec allows 5 but these tests do not
             return Err(Error::new(ErrorKind::Other, "q mod 6 should be = 1"));
         }
-        if !(p % 4 == 1) {
-            // spec allows 3 but ref C code does not
-            return Err(Error::new(ErrorKind::Other, "p mod 4 should be = 1"));
-        }
 
         Ok(NTRUPrime {
             params,
@@ -46,11 +41,39 @@ impl NTRUPrime {
         })
     }
 
-    pub fn encrypt(&self) {}
+    pub fn encrypt(&self, msg: &[u8]) {}
     pub fn decrypt(&self) {}
+
+    fn enc_len(&self) -> usize {
+        let (p, q, _, _) = self.params;
+
+        self.enc_len_pq(p, q)
+    }
+
+    fn enc_len_pq(&self, p: u16, q: u16) -> usize {
+        // Make sure q is a power of 2
+        if q & (q - 1) != 0 {
+            return 0;
+        }
+
+        let len_bits = p as f64 * (q as f64).log2();
+
+        let len_bytes = (len_bits + 7.0) / 8.0;
+
+        len_bytes as usize
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_enc_len_pq() {
+        let params = config::params::SNTRUP761;
+        let ntrup = NTRUPrime::from(params).unwrap();
+        let result = ntrup.enc_len_pq(1171, 2048);
+
+        assert!(result == 1611);
+    }
 }
