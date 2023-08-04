dcdf3e41d52fc05b74c72fd173ac4f19e459eec9
Hicaru <hicc0rus@gmail.com>
2023-08-04 14:11:41 +0500

    ntrup keygen

--- src/config/params.rs
+++ src/config/params.rs
@@ -6,3 +6,5 @@ pub const SNTRUP857: StartParams = (857, 5167, 322, 6553);
 pub const SNTRUP953: StartParams = (953, 6343, 396, 6553);
 pub const SNTRUP1013: StartParams = (1013, 7177, 448, 6553);
 pub const SNTRUP1277: StartParams = (1277, 7879, 492, 6553);
+
+pub struct NtruEncParams {}
