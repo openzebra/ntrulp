12dea1520d7f680e001fa21e89766c8981b411dd
hicaru <lich666black@gmail.com>
2023-08-25 21:10:03 +0500

    added verify

--- src/key/priv_key.rs
+++ src/key/priv_key.rs
@@ -1,20 +1,21 @@
 use crate::kem::r3::R3;
+use crate::kem::rq::Rq;
 
 #[derive(Debug)]
 pub struct PrivKey<const P: usize, const Q: usize, const Q12: usize> {
-    pub f: R3<P, Q, Q12>,
+    pub f: Rq<P, Q, Q12>,
     pub ginv: R3<P, Q, Q12>,
 }
 
 impl<const P: usize, const Q: usize, const Q12: usize> PrivKey<P, Q, Q12> {
     pub fn new() -> Self {
         Self {
-            f: R3::new(),
+            f: Rq::new(),
             ginv: R3::new(),
         }
     }
 
-    pub fn from(f: R3<P, Q, Q12>, ginv: R3<P, Q, Q12>) -> Self {
+    pub fn from(f: Rq<P, Q, Q12>, ginv: R3<P, Q, Q12>) -> Self {
         Self { f, ginv }
     }
 }
