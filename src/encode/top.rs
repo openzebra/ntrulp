35e00177c7b72583baee1be07931d59749f020f3
hicaru <lich666black@gmail.com>
2023-09-20 14:52:42 +0500

    added top method

--- src/encode/top.rs
+++ src/encode/top.rs
@@ -5,7 +5,7 @@ use crate::params::params1277::{P, PUBLICKEYS_BYTES, ROUNDED_BYTES, W};
 #[cfg(feature = "ntrulpr653")]
 use crate::params::params653::{P, PUBLICKEYS_BYTES, ROUNDED_BYTES, W};
 #[cfg(feature = "ntrulpr761")]
-use crate::params::params761::{P, TOP_BYTES};
+use crate::params::params761::{P, TAU0, TAU1};
 #[cfg(feature = "ntrulpr857")]
 use crate::params::params857::{P, PUBLICKEYS_BYTES, ROUNDED_BYTES, W};
 #[cfg(feature = "ntrulpr953")]
@@ -18,3 +18,28 @@ use crate::params::params953::{P, PUBLICKEYS_BYTES, ROUNDED_BYTES, W};
 //         s[i] = v as u8;
 //     }
 // }
+
+// static int8 Top(Fq C) { return (TAU1 * (int32)(C + TAU0) + 16384) >> 15; }
+
+pub fn top(c: i16) -> i8 {
+    let tau0 = TAU0 as i32;
+    let tau1 = TAU1 as i32;
+    let c32 = c as i32;
+    let value = (tau1 * (c32 + tau0) + 16384) >> 15;
+
+    value as i8
+}
+
+#[cfg(test)]
+mod tests_top {
+    use super::*;
+
+    #[test]
+    fn test_top() {
+        assert_eq!(top(4325), 23);
+        assert_eq!(top(0), 8);
+        assert_eq!(top(-30), 7);
+        assert_eq!(top(i16::MAX), 121);
+        assert_eq!(top(i16::MIN), -106);
+    }
+}
