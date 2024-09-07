4bf6d6813e2843c00bf12a904e610b8be139c7c0
Rinat <hicc0rus@gmail.com>
2024-09-07 17:38:40 +0500

    added test for std encrypt/decrtypt

--- src/ntru/std_cipher.rs
+++ src/ntru/std_cipher.rs
@@ -1,9 +1,9 @@
 use rand::RngCore;
 
-use crate::params::params::{P, R3_BYTES, RQ_BYTES, W};
+use crate::params::params::{P, RQ_BYTES};
 
 use crate::compress::compress;
-use crate::encode::{r3, rq};
+use crate::encode::rq;
 use crate::key::priv_key::PrivKey;
 use crate::key::pub_key::PubKey;
 use crate::poly::r3::R3;
@@ -47,4 +47,103 @@ pub fn bytes_decrypt(bytes: &[u8], priv_key: &PrivKey) -> Result<Vec<u8>, Cipher
     let out_r3 = compress::r3_merge_w_chunks(&r3_chunks, &size, seed);
 
     Ok(compress::r3_encode_chunks(&out_r3))
+}
+
+#[cfg(test)]
+mod test_cipher_std {
+    use crate::rng::{random_small, short_random};
+
+    use super::*;
+
+    #[test]
+    fn test_bytes_cipher() {
+        let mut rng = rand::thread_rng();
+        let mut g: R3;
+        let mut ciphertext = vec![0u8; 1024];
+        rng.fill_bytes(&mut ciphertext);
+        let f: Rq = Rq::from(short_random(&mut rng).unwrap());
+        let sk = loop {
+            g = R3::from(random_small(&mut rng));
+
+            match PrivKey::compute(&f, &g) {
+                Ok(s) => break s,
+                Err(_) => continue,
+            };
+        };
+        let pk = PubKey::compute(&f, &g).unwrap();
+        let mut encrypted = bytes_encrypt(&mut rng, &ciphertext, &pk);
+        let encrypted1 = bytes_encrypt(&mut rng, &ciphertext, &pk);
+        assert_ne!(encrypted, encrypted1);
+        let decrypted = bytes_decrypt(&encrypted, &sk).unwrap();
+
+        assert_eq!(decrypted, ciphertext.to_vec());
+
+        encrypted[2] = 0;
+        encrypted[1] = 0;
+        encrypted[3] = 0;
+        encrypted[4] = 0;
+        encrypted[5] = 0;
+        encrypted[6] = 0;
+
+        let decrypted = bytes_decrypt(&encrypted, &sk).unwrap();
+
+        assert_ne!(decrypted, ciphertext.to_vec());
+    }
+
+    #[test]
+    fn test_invalid_keys() {
+        let mut rng = rand::thread_rng();
+        let mut g: R3;
+        let mut ciphertext = vec![0u8; 1024];
+        rng.fill_bytes(&mut ciphertext);
+        let f: Rq = Rq::from(short_random(&mut rng).unwrap());
+        loop {
+            g = R3::from(random_small(&mut rng));
+
+            match PrivKey::compute(&f, &g) {
+                Ok(_) => break,
+                Err(_) => continue,
+            };
+        }
+        let pk = PubKey::compute(&f, &g).unwrap();
+        let invalid_sk = loop {
+            g = R3::from(random_small(&mut rng));
+
+            match PrivKey::compute(&f, &g) {
+                Ok(s) => break s,
+                Err(_) => continue,
+            };
+        };
+        let encrypted = bytes_encrypt(&mut rng, &ciphertext, &pk);
+        let decrypted = bytes_decrypt(&encrypted, &invalid_sk).unwrap();
+
+        assert_ne!(decrypted, ciphertext);
+    }
+
+    #[test]
+    fn test_invalid_bytes_decrypt() {
+        let mut rng = rand::thread_rng();
+        let mut g: R3;
+        let mut invalid_bytes = vec![0u8; 128];
+        rng.fill_bytes(&mut invalid_bytes);
+        let f: Rq = Rq::from(short_random(&mut rng).unwrap());
+        loop {
+            g = R3::from(random_small(&mut rng));
+
+            match PrivKey::compute(&f, &g) {
+                Ok(_) => break,
+                Err(_) => continue,
+            };
+        }
+        let invalid_sk = loop {
+            g = R3::from(random_small(&mut rng));
+
+            match PrivKey::compute(&f, &g) {
+                Ok(s) => break s,
+                Err(_) => continue,
+            };
+        };
+
+        assert!(bytes_decrypt(&invalid_bytes, &invalid_sk).is_err());
+    }
 }
