435a2c2a17d1d4604c23a0bbc5fc67ab14d53d36
Rinat <hicc0rus@gmail.com>
2024-09-08 16:27:55 +0500

    added docs

--- src/ntru/cipher.rs
+++ src/ntru/cipher.rs
@@ -6,6 +6,50 @@ use crate::{
     poly::{f3::round, r3::R3, rq::Rq},
 };
 
+/// Decrypts a polynomial in the Fq field using a private key.
+///
+/// # Arguments
+///
+/// - `c`: The ciphertext polynomial to be decrypted.
+/// - `priv_key`: The private key used for decryption.
+///
+/// # Returns
+///
+/// Returns the decrypted polynomial as a result of applying the private key to the ciphertext.
+///
+/// # Example
+///
+/// ```rust
+/// use rand::RngCore;
+/// use ntrulp::key::priv_key::PrivKey;
+/// use ntrulp::poly::rq::Rq;
+/// use ntrulp::poly::r3::R3;
+/// use ntrulp::ntru::cipher::rq_decrypt;
+/// use ntrulp::rng::{random_small, short_random};
+///
+/// let mut rng = rand::thread_rng();
+/// let f = Rq::from(short_random(&mut rng).unwrap());
+/// let mut g: R3;
+///
+/// // Generate the ciphertext polynomial c and the private key
+/// let c = Rq::from(short_random(&mut rng).unwrap());
+/// let priv_key = loop {
+///     g = R3::from(random_small(&mut rng));
+///     match PrivKey::compute(&f, &g) {
+///         Ok(s) => break s,
+///         Err(_) => continue,
+///     };
+/// };
+///
+/// // Decrypt the ciphertext polynomial
+/// let decrypted_poly = rq_decrypt(&c, &priv_key);
+/// ```
+///
+/// # Notes
+///
+/// This function decrypts a ciphertext polynomial `c` using a private key `priv_key`
+/// and returns the decrypted polynomial.
+///
 pub fn rq_decrypt(c: &Rq, priv_key: &PrivKey) -> R3 {
     let f = &priv_key.0;
     let ginv = &priv_key.1;
@@ -27,6 +71,57 @@ pub fn rq_decrypt(c: &Rq, priv_key: &PrivKey) -> R3 {
     R3::from(r)
 }
 
+/// Encrypts a polynomial in the F3 field using a public key in the Fq field.
+///
+/// # Arguments
+///
+/// - `r`: The polynomial to be encrypted in the F3 field.
+/// - `pub_key`: The public key used for encryption in the Fq field.
+///
+/// # Returns
+///
+/// Returns the ciphertext polynomial as a result of applying the public key to `r`.
+///
+/// # Example
+///
+/// ```rust
+/// use rand::RngCore;
+/// use ntrulp::key::priv_key::PrivKey;
+/// use ntrulp::poly::rq::Rq;
+/// use ntrulp::poly::r3::R3;
+/// use ntrulp::ntru::cipher::rq_decrypt;
+/// use ntrulp::ntru::cipher::r3_encrypt;
+/// use ntrulp::key::pub_key::PubKey;
+/// use ntrulp::rng::{random_small, short_random};
+///
+/// let mut rng = rand::thread_rng();
+/// let f = Rq::from(short_random(&mut rng).unwrap());
+/// let mut g: R3;
+///
+/// // Generate an content for encrypt
+/// let r: R3 = Rq::from(short_random(&mut rng).unwrap()).r3_from_rq();
+///
+/// // Generate the private key priv_key
+/// let priv_key = loop {
+///     g = R3::from(random_small(&mut rng));
+///     match PrivKey::compute(&f, &g) {
+///         Ok(s) => break s,
+///         Err(_) => continue,
+///     };
+/// };
+/// let pub_key = PubKey::from_sk(&priv_key).unwrap();
+///
+/// let encrypted = r3_encrypt(&r, &pub_key);
+/// let decrypted = rq_decrypt(&encrypted, &priv_key);
+///
+/// assert_eq!(decrypted.coeffs, r.coeffs);
+/// ```
+///
+/// # Notes
+///
+/// This function encrypts a polynomial `r` in the F3 field using a public key `pub_key`
+/// in the Fq field and returns the ciphertext polynomial.
+///
 pub fn r3_encrypt(r: &R3, pub_key: &PubKey) -> Rq {
     let mut hr = pub_key.mult_r3(r);
 
@@ -35,10 +130,117 @@ pub fn r3_encrypt(r: &R3, pub_key: &PubKey) -> Rq {
     hr
 }
 
+/// Encrypts a slice of bytes using the provided `NTRURandom` instance and the recipient's public key.
+///
+/// # Arguments
+///
+/// * `rng`: An instance of `NTRURandom` used for encryption.
+/// * `bytes`: A slice of bytes to be encrypted.
+/// * `pub_key`: The public key of the recipient.
+///
+/// # Returns
+///
+/// Returns the encrypted bytes.
+///
+/// # Example
+/// ```rust
+/// use rand::RngCore;
+/// use ntrulp::key::priv_key::PrivKey;
+/// use ntrulp::poly::rq::Rq;
+/// use ntrulp::poly::r3::R3;
+/// use ntrulp::ntru::cipher::static_bytes_encrypt;
+/// use ntrulp::ntru::cipher::static_bytes_decrypt;
+/// use ntrulp::key::pub_key::PubKey;
+/// use ntrulp::rng::{random_small, short_random};
+///
+/// let mut rng = rand::thread_rng();
+/// let f = Rq::from(short_random(&mut rng).unwrap());
+/// let mut g: R3;
+///
+/// // Generate the private key priv_key
+/// let sk = loop {
+///     g = R3::from(random_small(&mut rng));
+///     match PrivKey::compute(&f, &g) {
+///         Ok(s) => break s,
+///         Err(_) => continue,
+///     };
+/// };
+///
+/// // Generate an content for encrypt
+///
+/// let pk = PubKey::from_sk(&sk).unwrap();
+/// let plaintext = Rq::from(short_random(&mut rng).unwrap())
+///     .r3_from_rq()
+///     .to_bytes();
+///
+/// let encrypted = static_bytes_encrypt(&plaintext, &pk);
+/// let decrypted = static_bytes_decrypt(&encrypted, &sk);
+///
+/// assert_eq!(decrypted, plaintext);
+///
+/// ```
+///
+/// # Panics
+///
+/// The function may panic if encryption fails or if the provided public key is invalid.
+///
 pub fn static_bytes_encrypt(bytes: &[u8; R3_BYTES], pub_key: &PubKey) -> [u8; RQ_BYTES] {
     r3_encrypt(&bytes.into(), pub_key).to_bytes()
 }
 
+/// Decrypts bytes and retrieves the bytes previously encrypted using the `bytes_encrypt` function.
+///
+/// # Arguments
+///
+/// * `bytes`: A slice of bytes to decrypt.
+/// * `priv_key`: The private key used for decryption.
+///
+/// # Returns
+///
+/// Returns the decrypted bytes.
+///
+/// # Example
+///
+/// ```rust
+/// use rand::RngCore;
+/// use ntrulp::key::priv_key::PrivKey;
+/// use ntrulp::poly::rq::Rq;
+/// use ntrulp::poly::r3::R3;
+/// use ntrulp::ntru::cipher::static_bytes_encrypt;
+/// use ntrulp::ntru::cipher::static_bytes_decrypt;
+/// use ntrulp::key::pub_key::PubKey;
+/// use ntrulp::rng::{random_small, short_random};
+///
+/// let mut rng = rand::thread_rng();
+/// let f = Rq::from(short_random(&mut rng).unwrap());
+/// let mut g: R3;
+///
+/// // Generate the private key priv_key
+/// let sk = loop {
+///     g = R3::from(random_small(&mut rng));
+///     match PrivKey::compute(&f, &g) {
+///         Ok(s) => break s,
+///         Err(_) => continue,
+///     };
+/// };
+///
+/// // Generate an content for encrypt
+///
+/// let pk = PubKey::from_sk(&sk).unwrap();
+/// let plaintext = Rq::from(short_random(&mut rng).unwrap())
+///     .r3_from_rq()
+///     .to_bytes();
+///
+/// let encrypted = static_bytes_encrypt(&plaintext, &pk);
+/// let decrypted = static_bytes_decrypt(&encrypted, &sk);
+///
+/// assert_eq!(decrypted, plaintext);
+/// ```
+///
+/// # Panics
+///
+/// The function may panic if decryption fails or if the private key is invalid.
+///
 pub fn static_bytes_decrypt(cipher_bytes: &[u8; RQ_BYTES], priv_key: &PrivKey) -> [u8; R3_BYTES] {
     rq_decrypt(&cipher_bytes.into(), priv_key).to_bytes()
 }
